0ce5ef518eb4a6e5aa94ee72e8d1710c4fd5e17d
大森 龍人 <106291438+RyutoOhmori0414@users.noreply.github.com>
2024-02-17 23:30:29 +0900

    [add] effectも追加

--- Assets/Game/Shader/Library/Macro.hlsl
+++ Assets/Game/Shader/Library/Macro.hlsl
@@ -1,6 +1,8 @@
 #ifndef TOON_SHADER_MACRO_INCLUDED
 #define TOON_SHADER_MACRO_INCLUDED
 
+#define remap(value, inMin, inMax, outMin, outMax) (value - inMin) * ((outMax - outMin) / (inMax - inMin)) + outMin
+
 // デフォルト関数の拡張
 #define max3(a, b, c) max(max(a, b), c)
 #define max4(a, b, c, d) max(max(a, b), max(c, d))
